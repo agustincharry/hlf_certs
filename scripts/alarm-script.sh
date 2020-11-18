efe8fe8d30f7b3c7927198ea8fa4edae0cbe11b5
ccrios <ccrios@hotmail.es>
2020-11-18 17:05:36 -0500

    se agrega accion a la alarma

--- scripts/alarm-script.sh
+++ scripts/alarm-script.sh
@@ -2,8 +2,9 @@
 instanceTagName=$1
 nameSpace=$2
 region=$3
+arnTopic=$4
 aws cloudwatch put-metric-alarm  --region $region --alarm-name "Container status $instanceTagName" \
     --alarm-description "If the container named $instanceTagName is down this alarm is triggered" \
     --metric-name "Container status $instanceTagName" --namespace $nameSpace \
     --statistic Average --period 30 --threshold 1 --comparison-operator LessThanThreshold \
-    --evaluation-periods 1
+    --evaluation-periods 1  --alarm-actions $arnTopic
