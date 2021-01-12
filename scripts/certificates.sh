f7e84c25d7b3e93ebf37ce99f508afccedc9f4ad
ccrios <ccrios@hotmail.es>
2021-01-11 20:38:09 -0500

    se agrega certificate scripts

--- scripts/certificates.sh
+++ scripts/certificates.sh
@@ -3,7 +3,7 @@ openssl req -new -key private.pem -out request1.csr -subj "/C=CO/ST=ANTIOQUIA/L=
 
 echo -----solicitud -----
 #ARN_CERTIFICATE=$(aws acm-pca issue-certificate --region "us-east-1" --certificate-authority-arn arn:aws:acm-pca:us-east-1:872308410481:certificate-authority/ee2eadae-1a4e-4034-9f22-cc2626854c20 --csr file://./request1.csr --signing-algorithm "SHA256WITHECDSA" --template-arn "arn:aws:acm-pca:::template/EndEntityCertificate/V1" --validity Value=365,Type="DAYS" --profile acm-pca-blockchain | jq -r .CertificateArn)
-$ARN_CERTIFICATE="TEST"
+ARN_CERTIFICATE="TEST"
 echo $ARN_CERTIFICATE ---
 #aws acm-pca get-certificate --region "us-east-1" --certificate-authority-arn arn:aws:acm-pca:us-east-1:872308410481:certificate-authority/ee2eadae-1a4e-4034-9f22-cc2626854c20 --certificate-arn $ARN_CERTIFICATE --profile acm-pca-blockchain
 
